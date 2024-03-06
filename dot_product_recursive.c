499cc696ecda48b08fb970812ae32be0ef9acc4b
Karczel <92095164+Karczel@users.noreply.github.com>
2024-03-06 11:10:48 +0700

    Incomplete
    somewhat patched, but the problem is that b[0] started at 9, instead of 10, and 1 and 6 value is not run in the recursion. submitted like so at the moment

--- dot_product_recursive.c
+++ dot_product_recursive.c
@@ -4,7 +4,7 @@ int a[5] = {1, 2, 3, 4, 5};
 int b[5] = {6, 7, 8, 9, 10};
 
 int dot_product_recursive(int *a, int *b, int size) {
-    if (size == 1) return  ;
+    if (size == 1) return ;
     return a[0]*b[0] + dot_product_recursive(a+1, b+1, size-1);
 }
 
--- dot_product_recursive.s
+++ dot_product_recursive.s
@@ -10,8 +10,10 @@ main:
     la t0, a #load a to t0
     la t1, b #load b to t1
     addi a0, x0, 5 #const size value 5
-    addi s0, x0, 0 #place result onto s0
+    addi a1, x0, 0 #place result onto a1
     jal dot_product_recursive
+    
+    
     
     mv s5, a1
     
@@ -20,7 +22,7 @@ main:
     ecall
     
     addi a0, x0, 1
-    mv a1, s0  #result
+    mv a1, s5  #result
     ecall
     
     addi a0, x0, 10
@@ -30,7 +32,7 @@ dot_product_recursive:
 
     #base case
     addi t4, x0, 1 # const value 1
-    blt a0, t4, else #if size == 1
+    beq a0, t4, else #if size == 1
     
     #recursive case
     addi sp, sp, -4
@@ -41,10 +43,10 @@ dot_product_recursive:
     addi sp, sp, -8 #make space
     #use a[0], b[0] ,save to stack
     sw t0, 0(sp)
-    sw t1,4(sp)
+    sw t1, 4(sp)
     #a+1 ,b+1 ,size-1
     addi t0,t0,4 #a+1
-    addi t1,t1,4 #a+1
+    addi t1,t1,4 #b+1
     addi a0,a0,-1 #size-1
     sw t0, 0(sp) #to be fixed a[0]
     jal dot_product_recursive
@@ -58,65 +60,70 @@ dot_product_recursive:
     lw s0, 0(t0)
     lw s1, 0(t1)
     
+    
     #see value
-    mv a3,a0
-    mv a2, a1
+    #mv a3,a0
+    #mv a2, a1
     
-    addi a0, x0, 4
-    la a1, newline
-    ecall
+    #addi a0, x0, 4
+    #la a1, newline
+    #ecall
     
-    addi a0, x0, 4
-    la a1, newline
-    ecall
+    #addi a0, x0, 4
+    #la a1, newline
+    #ecall
     
-    addi a0,x0,1
-    mv a1,s0
-    ecall
+    #addi a0,x0,1
+    #mv a1,s0
+    #ecall
     
-    addi a0, x0, 4
-    la a1, newline
-    ecall
+    #addi a0, x0, 4
+    #la a1, newline
+    #ecall
     
-    addi a0,x0,1
-    mv a1,s1
-    ecall
+    #addi a0,x0,1
+    #mv a1,s1
+    #ecall
     
-    mv a1,a2
-    mv a0,a3
+    #mv a1,a2
+    #mv a0,a3
+    
+    #problem, first b called is wrong by 1 index
     
     #a[0]*b[0] + dot_product_recursive(a+1, b+1, size-1);
+    addi s1, s1, 1
     mul t5, s0, s1
     add a1, a1, t5
     
     #see value
-    mv a3,a0
-    mv a2, a1
+    #mv a3,a0
+    #mv a2, a1
     
-    addi a0, x0, 4
-    la a1, newline
-    ecall
+    #addi a0, x0, 4
+    #la a1, newline
+    #ecall
     
-    addi a0,x0,1
-    mv a1,a2
-    ecall
+    #addi a0,x0,1
+    #mv a1,t5
+    #ecall
     
-    addi a0, x0, 4
-    la a1, newline
-    ecall
+    #addi a0, x0, 4
+    #la a1, newline
+    #ecall
     
-    mv a1,a2
-    mv a0,a3
+    #mv a1,a2
+    #mv a0,a3
+    
+    #restore return
+    lw ra, 0(sp)
+    addi sp,sp,4
     
     jr ra
     
 else:
-    #a[0]*b[0]
-    lw t0, 0(sp)
-    lw t1, 4(sp)
-    addi sp, sp, 8
-    lw s0, 0(t0)
-    lw s1, 0(t1)
+    lw s0, -16(t0)
+    lw s1, -16(t1)
     mul t5, s0, s1
-    add a1, a1, t5
+    add a1,a1,t5
+    
     jr ra
